8d19f3d7d8c336b646323b05c8411fdbd5c2ed39
Witold Wojnarowicz <witold.wojnarowicz@gmail.com>
2025-01-30 13:04:17 +0100

    CR-002: Add SpringBoot module

---
+++ documentmngr-app/src/main/java/com/veeteq/documentmngr/DocumentMngrApp.java
@@ -0,0 +1,12 @@
+package com.veeteq.documentmngr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentMngrApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentMngrApp.class, args);
+    }
+}
