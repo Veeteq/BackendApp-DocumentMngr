312f2f1e1aced2897e0a7996a360403b8969f8b9
Witold Wojnarowicz <witold.wojnarowicz@gmail.com>
2025-01-30 09:48:02 +0100

    Initial commit

---
+++ src/test/java/com/veeteq/document_mngr/FinancialDocumentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veeteq.document_mngr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDocumentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
