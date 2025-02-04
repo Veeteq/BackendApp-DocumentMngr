4584f9a34af21558632f7ea62fdbd7cc6c276206
Witold Wojnarowicz <witold.wojnarowicz@gmail.com>
2025-02-04 22:00:41 +0100

    CR-009: Configure Swagger Request Validator

---
+++ documentmngr-app/src/test/java/com/veeteq/documentmngr/config/OpenApiValidationConfig.java
@@ -0,0 +1,42 @@
+package com.veeteq.documentmngr.config;
+
+import com.atlassian.oai.validator.OpenApiInteractionValidator;
+import com.atlassian.oai.validator.springmvc.OpenApiValidationFilter;
+import com.atlassian.oai.validator.springmvc.OpenApiValidationInterceptor;
+import com.atlassian.oai.validator.springmvc.SpringMVCLevelResolverFactory;
+import jakarta.servlet.Filter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.IOException;
+
+@Configuration
+public class OpenApiValidationConfig {
+
+    @Bean
+    public Filter validationFilter() {
+        return new OpenApiValidationFilter(
+                true, // enable request validation
+                true  // enable response validation
+        );
+    }
+
+    @Bean
+    public WebMvcConfigurer addOpenApiValidationInterceptor(@Value("${open.api.spec.url}") final String apiSpecification) throws IOException {
+        final OpenApiInteractionValidator validator = OpenApiInteractionValidator
+                .createForSpecificationUrl(apiSpecification)
+                .withLevelResolver(SpringMVCLevelResolverFactory.create())
+                .withBasePathOverride("/v1")
+                .build();
+        final OpenApiValidationInterceptor openApiValidationInterceptor = new OpenApiValidationInterceptor(validator);
+        return new WebMvcConfigurer() {
+            @Override
+            public void addInterceptors(final InterceptorRegistry registry) {
+                registry.addInterceptor(openApiValidationInterceptor);
+            }
+        };
+    }
+}
